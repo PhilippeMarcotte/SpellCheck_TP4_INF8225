d538f315c6a3700c8b7be2e58e1ea9562f8ddd3c
Olivier St-Amour <olivierstamour0952@gmail.com>
2017-04-16 10:34:17 -0400

    Script install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,21 @@ yum install git
 
 git clone https://esiode:in9el106e2y@bitbucket.org/inf8225/tp4.git
 
-cd tp4/dataset
+mkdir -p tp4/dataset/tar_archives
+
+cd tp4/dataset/tar_archives
 
 wget http://www.statmt.org/lm-benchmark/1-billion-word-language-modeling-benchmark-r13output.tar.gz
+
+cd ..
 
 md5sum tar_archives/training-monolingual.tgz 
 
 tar --extract -v --file tar_archives/training-monolingual.tgz --wildcards training-monolingual/news.20??.en.shuffled
 md5sum training-monolingual/*
 
-mkdir tmp.tmp
+source ./scripts/get-data.sh 
 
-TMPDIR=tmp.tmp ./get-data.sh 
+cd ..
 
-rmdir tmp.tmp
+python3 src/training.py
